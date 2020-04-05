07ee9985e8adf58887acf1203daa13a53cdc1d31
GuusLake <g.lakeman@student.rug.nl>
2020-04-05 15:50:07 +0200

    Added base template

---
+++ Assigment3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# File name: assignment3.py
+# Authors: Lakeman, G (s3383180) and Algra, N (s3133125)
+# Date: 05-04-20
+
+import tweepy
+import tkinter as tk
+from tkinter import ttk
+from tkinter import simpledialog
+from tkinter import messagebox
+import time
+import queue
+import threading
+
+def main():
+
+if __name__ == "__main__":
+    main()
