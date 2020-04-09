2ffd8c9ea84409a784b2d5d3422f94654bbec63b
GuusLake <g.lakeman@student.rug.nl>
2020-04-09 23:33:34 +0200

    Reworked variables input

--- Assigment3.py
+++ Assigment3.py
@@ -11,7 +11,7 @@ from tkinter import messagebox
 import time
 import queue
 import threading
-import geopy
+from geopy import Nominatim
 
 
 class CustomStream(tweepy.StreamListener):
@@ -53,6 +53,7 @@ class MyQueue():
 class Credentials():
     def __init__(self, queue):
         self.queue = queue
+        self.geolocator = Nominatim(user_agent = 'Guus en Nick')
         #temp
         self.lang = ['en']
         self.keywords = ['corona']
@@ -96,9 +97,23 @@ class Credentials():
         self.stream_listener = CustomStream(self.queue)
         self.stream = tweepy.Stream(auth = self.api.auth, listener = self.stream_listener)
         
-    def set_var(self, languages, keywords, loc_long, loc_lang):
-        self.lang = languages
-        self.keywords = keywords
+    def set_var(self, languages, keywords, location_string, radius):
+        if languagues:
+            self.lang = languages.split(', ')
+        else:
+            self.lang = False
+        self.keywords = keywords.split(', ')
+        self.radius = radius
+        if location_string:
+            try:
+                self.location = self.geolocator.geocode(location_string)
+                self.loc = [self.location.longitude, self.location.latitude]
+            except:
+                messagebox.showerror('Error: Location not found', 'The given location has not been found, no location will be used instead')
+                self.loc = False
+        else:
+            self.loc = False
+        
         
     def start_stream(self):
         print("Starting stream...")
@@ -106,21 +121,21 @@ class Credentials():
         print("Started stream?!")
 
 class IncomingTweets(tk.Frame):
-    ''' Main interface class for the reddit submission stream '''
+    ''' Main interface class for the Twitter stream '''
     def __init__(self, parent, api, queue):
         tk.Frame.__init__(self, parent)
         self.parent = parent
         self.api = api
         self.queue = queue
         self.dict = dict()
-        self.after(100, check_queue)
+        #self.after(100, check_queue)
 
         # Make menubar
         self.menubar = tk.Menu(self)
         self.filemenu = tk.Menu(self.menubar, tearoff=0)
         self.filemenu.add_command(label="Load conversation", command=exit)
-        self.filemenu.add_command(label="Close current tab", command=exit)
-        self.filemenu.add_command(label="exit", command=exit)
+        self.filemenu.add_command(label="Save conversation", command=exit)
+        self.filemenu.add_command(label="Exit", command=exit)
         self.menubar.add_cascade(label="File", menu=self.filemenu)
         self.accountmenu = tk.Menu(self.menubar, tearoff=0)
         self.accountmenu.add_command(label="Load credentials", command=lambda:self.api.set_new_cred())
@@ -128,26 +143,31 @@ class IncomingTweets(tk.Frame):
         self.parent.config(menu=self.menubar)
         
         # Input fields and buttons
-        self.lang_string = tk.StringVar()
-        self.lang_entry = tk.Entry(self, textvariable=self.lang_string)
-        self.lang_string.set("en, nl")
-        self.lang_label = tk.Label(self, text='Languages')
-        self.lang_label.grid(column=0, row=1, sticky= 'nswe')
-        self.lang_entry.grid(column=0, row=2, sticky= 'nswe')
+        self.langloc_string = tk.StringVar()
+        self.langloc_entry = tk.Entry(self, textvariable=self.langloc_string)
+        self.langloc_string.set("en, nl")
+        self.langloc_label = tk.Label(self, text='Languages')
+        self.langloc_label.grid(column=0, row=1, sticky= 'nsw')
+        self.langloc_entry.grid(column=0, row=2, columnspan = 2, sticky= 'nswe')
         
-        self.key_string = tk.StringVar()
-        self.key_entry = tk.Entry(self, textvariable=self.key_string)
-        self.key_string.set("Example, #List, @Input")
-        self.key_label = tk.Label(self, text='Keywords')
-        self.key_label.grid(column=0, row=3, sticky= 'nswe')
-        self.key_entry.grid(column=0, row=4, sticky= 'nswe')
+        self.keyrad_string = tk.StringVar()
+        self.keyrad_entry = tk.Entry(self, textvariable=self.keyrad_string)
+        self.keyrad_string.set("Example, #List, @Input")
+        self.keyrad_label = tk.Label(self, text='Keywords')
+        self.keyrad_label.grid(column=0, row=3, sticky= 'nsw')
+        self.keyrad_entry.grid(column=0, row=4, columnspan = 2, sticky= 'nswe')
+        self.old_loc = "Oude Kijk in Het Jatstraat 26 Groningen"
+        self.old_rad = '50'
         
-        self.loc_string = tk.StringVar()
-        self.loc_entry = tk.Entry(self, textvariable=self.loc_string)
-        self.loc_string.set("Oude Kijk in het Jatstraat 26 Groningen")
-        self.loc_label = tk.Label(self, text='Location')
-        self.loc_label.grid(column=0, row=5, sticky= 'nswe')
-        self.loc_entry.grid(column=0, row=6, sticky= 'nswe')
+        self.option_value = tk.IntVar()
+        self.option1 = tk.Radiobutton(self, text='Languages and Keywords', variable = self.option_value, value=1, command = lambda: self.set_lan_key())
+        self.option1.select()
+        self.option1.grid(column=0, row=5, sticky= 'nsw')
+        self.option2 = tk.Radiobutton(self, text='Location and Radius', variable = self.option_value, value=2, command = lambda: self.set_loc_rad())
+        self.option2.grid(column=0, row=6, sticky= 'nsw')
+        
+        self.var_button = tk.Button(self, text = "Start stream", command = lambda: self.set_variables())
+        self.var_button.grid(column=1, row=5, rowspan =2, sticky = 'nse', padx=15, pady=15)
         
         # Treeview with conversations
         self.tree = ttk.Treeview(self)
@@ -155,6 +175,23 @@ class IncomingTweets(tk.Frame):
         self.tree.configure(yscrollcommand=self.yscrollbarTree.set)
         self.yscrollbarTree.grid(row=0, column=0, sticky='nse')
         self.tree.grid(column=0, row=0, columnspan=3, sticky= 'nsew')
+
+    def set_lan_key(self):
+        self.old_loc = self.langloc_string.get()
+        self.langloc_string.set(self.old_lang)
+        self.langloc_label['text'] = 'Languages'
+        self.old_rad = self.keyrad_string.get()
+        self.keyrad_string.set(self.old_key)
+        self.keyrad_label['text'] = 'Keywords'
+        
+    def set_loc_rad(self):
+        self.old_lang = self.langloc_string.get()
+        self.langloc_string.set(self.old_loc)
+        self.langloc_label['text'] = 'Location'
+        self.old_key = self.keyrad_string.get()
+        self.keyrad_string.set(self.old_rad)
+        self.keyrad_label['text'] = 'Radius'
+        
 
     def check_queue():
         try:
@@ -171,6 +208,9 @@ class IncomingTweets(tk.Frame):
             return does_convo_exist(parent)
         else:
             return (status.id in self.dict.keys())
+            
+    def set_variables(self):
+        print(' test')
 
 
 def main():
